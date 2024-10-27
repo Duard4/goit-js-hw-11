2ac271987acd4972f65b1f254be899f07ca023af
Eduard <parhomenkoeduard75@gmail.com>
2024-10-27 15:35:20 +0200

    add: search bar, button, gallery, loader

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,54 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const search = document.querySelector('.search_bar');
+const button = document.querySelector('.button');
+const loader = document.querySelector('.loader');
+
+button.addEventListener('click', ImageSearch);
+
+const source = 'https://pixabay.com/api/?';
+const options = new URLSearchParams({
+	key: '46749030-b6cef6a6b69e043ecf4444c1b',
+	image_type: 'photo',
+	orientation: 'horizontal',
+	per_page: 50,
+});
+
+const error =
+	'Sorry, there are no images matching your search query. Please try again!';
+
+function ImageSearch() {
+	let searchValue = search.value.trim();
+	if (!searchValue) return;
+	options.set('q', searchValue);
+	loader.style.display = 'block';
+
+	fetch(`${source}${options}`)
+		.then(response => {
+			if (!response.ok) {
+				raiseError();
+			}
+			return response.json();
+		})
+		.then(posts => {
+			if (posts.total) {
+				const event = new CustomEvent('imagesFetched', { detail: posts.hits });
+				document.dispatchEvent(event);
+				loader.style.display = 'none';
+			} else {
+				raiseError();
+				loader.style.display = 'none';
+			}
+		})
+		.catch(error => console.log(error));
+}
+
+function raiseError() {
+	iziToast.error({
+		message: error,
+		position: 'topRight',
+		color: 'red',
+		theme: 'dark',
+	});
+}
---
+++ src/js/render-functions.js
@@ -0,0 +1,46 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+
+function renderGallery(images) {
+    let galleryHTML = "";
+    images.forEach((element) => {
+        galleryHTML += `
+            <li class="gallery_item">
+                <a class="gallery_link" href="${element.largeImageURL}" title="${element.tags}">
+                    <img
+                        class="gallery-image"
+                        src="${element.webformatURL}"
+                        alt="${element.tags}"
+                    />
+                </a>
+                <ul class="gallery_info">
+                    <li><span class="bold_text">Likes</span><p class="text">${element.likes}</p></li>
+                    <li><span class="bold_text">Views</span><p class="text">${element.views}</p></li>
+                    <li><span class="bold_text">Comments</span><p class="text">${element.comments}</p></li>
+                    <li><span class="bold_text">Downloads</span><p class="text">${element.downloads}</p></li>
+                </ul>
+            </li>
+        `;
+    });
+
+    gallery.innerHTML = galleryHTML;
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+        nav: true,
+        overlay: true,
+        captions: true,
+        captionSelector: 'self',
+        captionType: 'attr',
+        captionData: 'title',
+        captionPosition: 'bottom',
+        captionDelay: 250
+    });
+    
+    lightbox.refresh();
+}
+
+document.addEventListener('imagesFetched', (event) => {
+    renderGallery(event.detail);
+});
